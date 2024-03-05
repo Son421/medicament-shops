e1c2233a8583640d0a1fe7b2c6989155bd9b5740
Son421 <115635389+Son421@users.noreply.github.com>
2024-03-05 17:03:25 +0200

    add API for products list

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var shopsRouter = require('./routes/shops');
@@ -21,7 +20,6 @@ try {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
